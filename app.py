d5813e7cb80b4216824bdc59dae50c626ab70214
Muntasir Hossain <ahoss1@unh.newhaven.edu>
2024-04-16 23:06:16 -0500

    Clean pages

--- app.py
+++ app.py
@@ -1,19 +1,3 @@
-# import dash
-# from dash import html, dcc
-# import dash_bootstrap_components as dbc
-
-# app = dash.Dash(__name__, external_stylesheets=[dbc.themes.VAPOR],
-#                 meta_tags=[{'name': 'viewport',
-#                             'content': 'width=device-width, initial-scale=1.0'}], use_pages=True)
-
-# app.layout = html.Div([
-#     dcc.Location(id="url"),
-#     dash.page_container
-# ])
-
-# if __name__=='__main__':
-#     app.run_server(debug=True, port=8000)
-
 import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
